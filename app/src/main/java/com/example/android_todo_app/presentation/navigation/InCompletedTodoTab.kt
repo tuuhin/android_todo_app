f9e3e4d3511251fee423ce6e070523851148855d
tuuhin <tuhinbhowmick2513@gmail.com>
2022-08-25 21:21:46 +0530

    launcher icons has been changed

--- app/src/main/java/com/example/android_todo_app/presentation/navigation/InCompletedTodoTab.kt
+++ app/src/main/java/com/example/android_todo_app/presentation/navigation/InCompletedTodoTab.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
